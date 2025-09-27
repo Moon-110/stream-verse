8ac4ff5f422e3c764f427bce36b6352dec1ba8fa
Moon110 <moynul1.islam0@gmail.com>
2025-09-27 22:39:44 +0600

    load categories

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// 1. fetch, load and show categories on html
+// create loadCategories function
+const loadCategories = () => {
+    // fetch data
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then((res)=> res.json())
+    .then(data => displayCategories(data.categories))
+    .catch((error) => console.log(error))
+}
+
+// create displayCategories function
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById('categories')
+    
+    categories.forEach((item) => {
+        console.log(item)
+
+        // create a button
+        const button = document.createElement('button')
+        button.classList = 'btn'
+        button.innerText = item.category
+
+        // add button to category container
+        categoryContainer.append(button)
+    });
+}
+
+
+
+loadCategories()
+displayCategories()
