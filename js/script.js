8bd0cf038ddee887c4b898c0260857e3fa01986b
Moon110 <moynul1.islam0@gmail.com>
2025-09-28 13:49:54 +0600

    format time & display conditional time

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+function getTimeString(time){
+    const hour = parseInt(time/3600)
+    let remainingSecond = time%3600
+    const minute = parseInt(remainingSecond/60)
+    remainingSecond = remainingSecond%60;
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`
+}
+
 // 1. fetch, load and show categories on html
 // create loadCategories function
 const loadCategories = () => {
@@ -43,9 +51,12 @@ const displayVideos = (videos) =>{
      const card= document.createElement('div')
      card.classList = "card"
      card.innerHTML = `
-     <figure class="h-[200px]">
+     <figure class="h-[200px] relative">
     <img
       src=${videos.thumbnail} class="h-full w-full object-cover"/>
+
+      ${videos.others.posted_date?.length == 0 ? "": `<span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">${getTimeString(videos.others.posted_date)}</span>`}
+      
   </figure >
   <div class="px-0 py-2 flex gap-2">
     <div>
@@ -54,8 +65,9 @@ const displayVideos = (videos) =>{
     <div>
         <h2 class="font-bold">${videos.title}</h2>
         <div class="flex justify-center items-center gap-2">
-         <p class=""text-gray-400>${videos.authors[0].profile_name}</p>
-         <img class="w-5" src = "https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>
+         <p class="text-gray-400 py-0">${videos.authors[0].profile_name}</p>
+         
+         ${videos.authors[0].verified ==true ? '<img class="w-5" src = "https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>' : ''}
         </div>
        
         <p></p>
