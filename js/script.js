ca59ee107e5c58fde16b0ca1f5a2211d8aaa87cc
Moon110 <moynul1.islam0@gmail.com>
2025-09-28 12:36:16 +0600

    card added with title authors name

--- js/script.js
+++ js/script.js
@@ -7,6 +7,65 @@ const loadCategories = () => {
     .then(data => displayCategories(data.categories))
     .catch((error) => console.log(error))
 }
+// loadVideos
+const loadVideos = () => {
+    // fetch data
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then((res)=> res.json())
+    .then(data => displayVideos(data.videos))
+    .catch((error) => console.log(error))
+}
+
+const cardDemo ={
+    "category_id": "1001",
+    "video_id": "aaaa",
+    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+    "title": "Shape of You",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+            "profile_name": "Olivia Mitchell",
+            "verified": ""
+        }
+    ],
+    "others": {
+        "views": "100K",
+        "posted_date": "16278"
+    },
+    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+}
+
+const displayVideos = (videos) =>{
+    const videoContainer = document.getElementById('videos');
+    videos.forEach((videos) => {
+     console.log(videos)
+
+     const card= document.createElement('div')
+     card.classList = "card"
+     card.innerHTML = `
+     <figure class="h-[200px]">
+    <img
+      src=${videos.thumbnail} class="h-full w-full object-cover"/>
+  </figure >
+  <div class="px-0 py-2 flex gap-2">
+    <div>
+        <img class="w-10 h-10 rounded-full object-cover" src = ${videos.authors[0].profile_picture}/>
+    </div>
+    <div>
+        <h2 class="font-bold">${videos.title}</h2>
+        <div class="flex justify-center items-center gap-2">
+         <p class=""text-gray-400>${videos.authors[0].profile_name}</p>
+         <img class="w-5" src = "https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>
+        </div>
+       
+        <p></p>
+    </div>
+    </div>
+  </div>
+     `
+     videoContainer.append(card)
+    })
+}
 
 // create displayCategories function
 const displayCategories = (categories) => {
@@ -28,4 +87,4 @@ const displayCategories = (categories) => {
 
 
 loadCategories()
-displayCategories()
+loadVideos()
