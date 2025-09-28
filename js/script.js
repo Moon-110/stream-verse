11cc8bf9e1c8c84c25d7a06aaaa203bf62331b64
Moon110 <moynul1.islam0@gmail.com>
2025-09-28 14:42:41 +0600

    implement active button functionality

--- js/script.js
+++ js/script.js
@@ -5,6 +5,13 @@ function getTimeString(time){
     remainingSecond = remainingSecond%60;
     return `${hour} hour ${minute} minute ${remainingSecond} second ago`
 }
+const removeActiveClass=()=>{
+    const buttons = document.getElementsByClassName('category-btn')
+    console.log(buttons)
+    for(let btn of buttons){
+        btn.classList.remove('active')
+    }
+}
 
 // 1. fetch, load and show categories on html
 // create loadCategories function
@@ -29,7 +36,14 @@ const loadCategoriesVideos = (id) =>{
     // alert(id)
     fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
     .then((res)=> res.json())
-    .then(data => displayVideos(data.category))
+    .then(data => {
+        removeActiveClass()
+
+        const activeBtn=document.getElementById(`btn-${id}`)
+        activeBtn.classList.add('active')
+        displayVideos(data.category)
+
+    })
     .catch((error) => console.log(error))
 }
 
@@ -117,7 +131,7 @@ const displayCategories = (categories) => {
         // create a button
         const buttonContainer = document.createElement('div')
         buttonContainer.innerHTML=`
-        <button onclick="loadCategoriesVideos(${item.category_id})" class="btn">
+        <button id="btn-${item.category_id}" onclick="loadCategoriesVideos(${item.category_id})" class="btn category-btn">
         ${item.category}
 
         </button>
